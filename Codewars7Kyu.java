f11d4eecfaf15968d0eaf198b5f6b7bc544655af
Funt <funt_88@mail.ru>
2025-04-09 02:10:23 +0300

    Practic

--- Codewars7Kyu.java
+++ Codewars7Kyu.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 public class Codewars7Kyu {
     public static void main(String[] args) {
         int[] array = {15, 11, 10, 7, 12}; // нужно получить [15,7,12,10,11]
+        int square = 9119; // 9119 => 811181
 
         System.out.println(Arrays.toString(changeArr(array)));
+        System.out.println(toSquare(square));
+        System.out.println(toSquareV2(square));
     }
 
     // [15,11,10,7,12] => [15,7,12,10,11] Самое большое -> самое маленькое и тд.
@@ -22,6 +25,33 @@ public class Codewars7Kyu {
             }
         }
         return result;
+    }
+
+    // Цифры числа в квадрат 9119 => 811181
+    public static int toSquare(int num) {
+        String str = String.valueOf(num);
+        StringBuilder result = new StringBuilder();  // Для хранения результата
+        for (char ch : str.toCharArray()) {
+            int digit = ch - '0';  // Получаем цифру через вычитание символа '0'
+            int squared = digit * digit;  // Возводим цифру в квадрат
+            result.append(squared);  // Добавляем результат в строку
+        }
+        return Integer.parseInt(result.toString());
+    }
+
+    // Цифры числа в квадрат 9119 => 811181, второй способ
+    public static int toSquareV2(int num) {
+        if (num == 0) {  // Проверяем, если число равно 0
+            return 0;
+        }
+        StringBuilder result = new StringBuilder();  // Для хранения результата
+        while (num > 0) {
+            int digit = num % 10;  // Извлекаем последнюю цифру
+            int squared = digit * digit;  // Возводим цифру в квадрат
+            result.insert(0, squared);  // Добавляем результат в начало строки
+            num /= 10;  // Убираем последнюю цифру из числа
+        }
+        return Integer.parseInt(result.toString());  // Преобразуем результат обратно в число
     }
 }
 
